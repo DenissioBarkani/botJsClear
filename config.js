619bb390195e27e0ca248682c4810e749ad83718
DenissioBarkani <105939563+DenissioBarkani@users.noreply.github.com>
2022-08-07 18:21:51 +0300

    test

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    /** Bot owner(s)  */
+    owner: ['342935107591602176'],
+
+    /** Bot admin user ids, can reboot the bot and inherits support permissions */
+    admins: ['776092766328848386'],
+
+    /** Bot support user ids, can reload commands, display bot stats and inherits server owner permissions */
+    support: ['738522622110728194'],
+
+    /** Bot support server invite link */
+    invite: 'https://discord.com/api/oauth2/authorize?client_id=822900153751961611&permissions=4026400727&scope=bot',
+
+    /** Bot command prefix */
+    prefix: '=',
+
+    /** Error logging channel */
+    errorLogChannel: 'channel'
+};
